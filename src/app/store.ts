a4e5a2a45caf0a28f1421ff0f7308a81f69eb11e
Chris NeJame <chris.t.nejame@gmail.com>
2023-01-09 11:10:36 -0500

    feat: react redux boilerplate

---
+++ src/app/store.ts
@@ -0,0 +1,32 @@
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
+const rootReducer = combineReducers({});
+
+export function makeStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+const store = makeStore();
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action<string>
+>;
+
+export type AppState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>PPPP</title>
+      </Head>
+    </div>
+  );
+};
+
+export default IndexPage;
