09311d4790c95645db8fe490d6546035f5ef0ac1
Chris NeJame <chris.t.nejame@gmail.com>
2023-01-12 11:19:26 -0500

    fix: unneccessary export

--- src/Relations/index.ts
+++ src/Relations/index.ts
@@ -1,3 +1,2 @@
-export { default as IsolatedDependencyChain } from "./IsolatedDependencyChain";
 export { default as TaskUnit } from "./TaskUnit";
 export { default as TaskUnitCluster } from "./TaskUnitCluster";
