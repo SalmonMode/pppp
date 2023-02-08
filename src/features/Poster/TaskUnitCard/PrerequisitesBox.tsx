6a50cadeaf5b08b887b1e316e20523f2b5c978a2
Chris NeJame <chris.t.nejame@gmail.com>
2023-02-08 13:21:09 -0500

    feat: added icons to cards

--- src/features/Poster/TaskUnitCard/PrerequisitesBox.tsx
+++ src/features/Poster/TaskUnitCard/PrerequisitesBox.tsx
@@ -1,29 +1,52 @@
 import { css } from "@emotion/react";
 import { theme } from "../../../app/theme";
+import FactCheckOutlinedIcon from "@mui/icons-material/FactCheckOutlined";
+import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
 
 export default function PrerequisitesBox({ started }: { started: boolean }) {
-  const styles = started ? acceptedStyles : pendingStyles;
-  const className = started
-    ? "acceptedPrerequisiteBox"
-    : "pendingPrerequisiteBox";
-  return <div css={styles} className={`prerequisiteBox ${className}`}></div>;
+  const wrapperStyles = started ? acceptedStyles : pendingStyles;
+  const [className, IconComponent] = started
+    ? ["acceptedPrerequisiteBox", FactCheckOutlinedIcon]
+    : ["pendingPrerequisiteBox", PendingOutlinedIcon];
+  return (
+    <div css={wrapperStyles} className={`prerequisiteBox ${className}`}>
+      <div css={iconBoxStyles}>
+        <IconComponent fontSize="small" />
+      </div>
+    </div>
+  );
 }
 
 const baseStyles = css({
   width: theme.prerequisitesBoxWidth,
   flexShrink: 0,
   flexGrow: 0,
+  display: "flex",
   height: "100%",
+  flexDirection: "row",
+});
+const iconBoxStyles = css({
+  display: "flex",
+  height: "100%",
+  width: "100%",
+  justifyContent: "center",
+  alignItems: "center",
 });
 const acceptedStyles = css(
   {
     backgroundColor: theme.prereqsAcceptedColor,
+    "path,circle": {
+      color: theme.prereqsAcceptedIconColor,
+    },
   },
   baseStyles
 );
 const pendingStyles = css(
   {
     backgroundColor: theme.prereqsPendingColor,
+    "path,circle": {
+      color: theme.prereqsPendingIconColor,
+    },
   },
   baseStyles
 );
--- src/features/Poster/TaskUnitCard/ReviewBox.tsx
+++ src/features/Poster/TaskUnitCard/ReviewBox.tsx
@@ -1,11 +1,24 @@
 import { css, SerializedStyles } from "@emotion/react";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
+import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
+import ReportGmailerrorredOutlinedIcon from "@mui/icons-material/ReportGmailerrorredOutlined";
+import WarningAmberRoundedIcon from "@mui/icons-material/WarningAmberRounded";
+import type SvgIcon from "@mui/material/SvgIcon";
 import { theme } from "../../../app/theme";
 import { ReviewType } from "../../../types";
 
 export default function ReviewBox({ variant }: { variant: ReviewType }) {
   const className = classMap[variant];
-  const styles = styleMap[variant];
-  return <div css={styles} className={`reviewBox ${className}`}></div>;
+  const wrapperStyles = styleMap[variant];
+  const IconComponent = iconMap[variant];
+  return (
+    <div css={wrapperStyles} className={`reviewBox ${className}`}>
+      <div css={iconBoxStyles}>
+        <IconComponent fontSize="small" />
+      </div>
+    </div>
+  );
 }
 
 type ReviewVariantClassMap = {
@@ -13,6 +26,9 @@ type ReviewVariantClassMap = {
 };
 type ReviewVariantStyleMap = {
   [V in ReviewType]: SerializedStyles;
+};
+type ReviewVariantIconMap = {
+  [V in ReviewType]: typeof SvgIcon;
 };
 
 const classMap: ReviewVariantClassMap = {
@@ -27,38 +43,63 @@ const baseStyles = css({
   width: theme.reviewBoxWidth,
   flexShrink: 0,
   flexGrow: 0,
+  display: "flex",
   height: "100%",
+  flexDirection: "row",
 });
 const pendingStyles = css(
   {
     backgroundColor: theme.reviewPendingColor,
+    "path,circle": {
+      color: theme.reviewPendingIconColor,
+    },
   },
   baseStyles
 );
 const acceptedStyles = css(
   {
     backgroundColor: theme.reviewAcceptedColor,
+    "path,circle": {
+      color: theme.reviewAcceptedIconColor,
+    },
   },
   baseStyles
 );
 const minorStyles = css(
   {
     backgroundColor: theme.reviewMinorColor,
+    "path,circle": {
+      color: theme.reviewMinorIconColor,
+    },
   },
   baseStyles
 );
 const majorStyles = css(
   {
     backgroundColor: theme.reviewMajorColor,
+    "path,circle": {
+      color: theme.reviewMajorIconColor,
+    },
   },
   baseStyles
 );
 const rebuildStyles = css(
   {
     backgroundColor: theme.reviewRebuildColor,
+    "path,circle": {
+      color: theme.reviewRebuildIconColor,
+    },
   },
   baseStyles
 );
+
+const iconBoxStyles = css({
+  display: "flex",
+  height: "100%",
+  width: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+});
 
 const styleMap: ReviewVariantStyleMap = {
   [ReviewType.Pending]: pendingStyles,
@@ -66,4 +107,11 @@ const styleMap: ReviewVariantStyleMap = {
   [ReviewType.NeedsMinorRevision]: minorStyles,
   [ReviewType.NeedsMajorRevision]: majorStyles,
   [ReviewType.NeedsRebuild]: rebuildStyles,
+};
+const iconMap: ReviewVariantIconMap = {
+  [ReviewType.Pending]: PendingOutlinedIcon,
+  [ReviewType.Accepted]: CheckCircleOutlineRoundedIcon,
+  [ReviewType.NeedsMinorRevision]: WarningAmberRoundedIcon,
+  [ReviewType.NeedsMajorRevision]: ReportGmailerrorredOutlinedIcon,
+  [ReviewType.NeedsRebuild]: DeleteOutlineRoundedIcon,
 };
